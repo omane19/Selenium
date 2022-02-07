ddf30efa07fd68cb5b5d1e73d3d7754bc697da72
omane19 <omkar.mane.90@gmail.com>
2022-02-07 01:05:35 -0600

    chai

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,4 +1,6 @@
 const{Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
+var should  = require("chai").should();
 
 
 async function example(){
@@ -11,6 +13,13 @@ await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
 //add a todo 
 await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+//assert
+let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+    return value
+});
+
+assert.strictEqual(todoText,"Learn Selenium");
 
 // close the browser 
 await driver.quit();
